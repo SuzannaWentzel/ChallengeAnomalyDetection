5067f65470707eec6b5e7317f295e1754fd2394d
sieta <s.dejong-3@student.utwente.nl>
2019-09-05 15:28:35 +0200

    Commented on MainActivity

--- ChallengeAnomalyDetection/Practicing/app/src/main/java/com/example/practicing/MainActivity.java
+++ ChallengeAnomalyDetection/Practicing/app/src/main/java/com/example/practicing/MainActivity.java
@@ -21,58 +21,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-//
-//    private SensorManager sensorManager;
-//    Sensor accelerometer;
-//
-//    TextView zAccValue;
-//
-//    private boolean toMap;
 
     @Override
+// Sets up the app and starts the functions that are needed for the app.
     protected void onCreate(Bundle savedInstanceState) {
-//        toMap = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        zAccValue = (TextView) findViewById(R.id.zAccValue);
-
-//        Log.d(TAG, "onCreate: Initializing Sensor services");
-//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//
-//        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        sensorManager.registerListener(MainActivity.this, accelerometer, sensorManager.SENSOR_DELAY_NORMAL);
-//        Log.d(TAG, "onCreate: Registered gyroscope and accelerometer");
-
+// Creates a button to start tracking the potholes.
         Button btn = (Button) findViewById(R.id.openMaps);
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
         btn.setOnClickListener(new View.OnClickListener() {
+// Whenever the button is clicked, MapsActivity is called and started.
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
-//                toMap = false;
                 finish();
             }
         });
     }
-
-//    @Override
-//    public void onAccuracyChanged(Sensor sensor, int i) {
-//
-//    }
-
-//    @Override
-//    public void onSensorChanged(SensorEvent sensorEvent) {
-//        Sensor sensor = sensorEvent.sensor;
-//
-//        if (!toMap) {
-//            finish();
-//        }
-//
-//        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-//            Log.d(TAG, "onAcceleroSensorChanged: X: " + sensorEvent.values[0] + ", Y: " + sensorEvent.values[1] + ", Z:  " + sensorEvent.values[2]);
-//
-//            zAccValue.setText("Z: " + sensorEvent.values[2]);
-//        }
-//    }
 }
